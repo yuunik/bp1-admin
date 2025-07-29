fb861f768a7e7dde2d2c5abc95b99a9d80e5dda3
zhou <zhou.z@proteus-dt.com>
2025-07-29 10:32:34 +0800

    feat: 添加请求拦截器以处理错误信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL_API,
@@ -16,9 +17,38 @@ request.interceptors.request.use(
 
 request.interceptors.response.use(
   (response) => {
+    const {
+      data: { code, msg },
+    } = response
+    // 若不是成功状态码, 则提示错误信息
+    if (code !== 0) {
+      // 错误提示
+      ElMessage.error(msg)
+    }
     return response.data
   },
   (error) => {
+    // 处理网络错误的情况
+    let msg = ''
+    const status = error.response?.status
+    switch (status) {
+      case 401:
+        msg = 'Token expired'
+        break
+      case 403:
+        msg = 'No access rights'
+        break
+      case 404:
+        msg = 'Request address error'
+        break
+      case 500:
+        msg = 'Server internal error'
+        break
+      default:
+        msg = 'No network connection'
+    }
+    // 错误提示
+    ElMessage.error(msg)
     return Promise.reject(error)
   },
 )
