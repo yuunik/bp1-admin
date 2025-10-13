80dc0b61f4aa9004024ff33c7e0cf482244a781b
zhou <zhou.z@proteus-dt.com>
2025-10-13 10:54:54 +0800

    style(PushNotifications): 更新页面布局以适应全高显示

--- src/views/AppConfiguration/PushNotifications/index.vue
+++ src/views/AppConfiguration/PushNotifications/index.vue
@@ -597,7 +597,7 @@ initData()
 </script>
 
 <template>
-  <section class="flex flex-col gap-16 px-32 pb-32">
+  <section class="flex h-full flex-col gap-16 px-32 pb-32">
     <!-- header -->
     <div class="flex-between h-32">
       <h2 class="heading-h2-20px-medium text-neutrals-off-black">
