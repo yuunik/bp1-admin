2311f3c112b3e4a7b64e1bb7eb75fa9059ce51e8
zhou <zhou.z@proteus-dt.com>
2025-10-21 14:48:25 +0800

    refactor(Layout): 移除路由视图的固定样式类

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -261,7 +261,7 @@ provide('isMenuCollapsed', isMenuCollapsed)
       <!-- breadcrumb -->
       <breadcrumb class="shrink-0" />
       <!-- router view -->
-      <router-view class="flex-1 overflow-auto" />
+      <router-view />
       <!-- 设置中心 -->
       <aside
         class="z-999 w-150 rounded-8 shadow-default bg-neutrals-white text-neutrals-off-black absolute bottom-8 left-8 flex flex-col gap-8 p-8 [&>span]:cursor-pointer"
