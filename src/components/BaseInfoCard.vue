ec296beac1065ab7882b37456fdb40db8dd607fc
zhou <zhou.z@proteus-dt.com>
2025-10-24 17:04:42 +0800

    feat(components): 添加基础信息卡片组件

---
+++ src/components/BaseInfoCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineProps } from 'vue'
+
+import { getFullFilePath } from '@/utils/dataFormattedUtil.js'
+
+// 接收外部传入的 logo 和 name
+defineProps({
+  logo: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <div class="flex items-center">
+    <el-avatar
+      v-if="logo"
+      :size="20"
+      :src="getFullFilePath(logo)"
+      alt="brand icon"
+      class="mr-8 h-20 w-20"
+      fit="cover"
+      shape="circle"
+      @error="() => true"
+    >
+      <template #error>
+        <i class="i-ep:picture" />
+      </template>
+    </el-avatar>
+    <el-text>{{ name || '-' }}</el-text>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
