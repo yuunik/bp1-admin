d9c7adbd9f3cd3aecb3c41dbbd6ee76f9eb8683f
zhou <zhou.z@proteus-dt.com>
2025-10-31 17:57:39 +0800

    feat(Share): 更换字体为 Poppins 并更新样式配置

--- src/share/index.vue
+++ src/share/index.vue
@@ -6,10 +6,18 @@
 
 <style scoped lang="scss">
 // Robot 字体
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+//@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+//
+//.h5-container {
+//  @apply h-full;
+//  font-family: 'Roboto', sans-serif;
+//}
+
+/* Poppins 字体 */
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 
 .h5-container {
   @apply h-full;
-  font-family: 'Roboto', sans-serif;
+  font-family: 'Poppins', sans-serif;
 }
 </style>
